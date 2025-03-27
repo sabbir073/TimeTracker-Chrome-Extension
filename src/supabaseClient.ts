4830256b345ec77dbfbd8a5e2af34649451570a3
Md Sabbir Ahmed <md.sabbir073@gmail.com>
2025-03-27 14:42:52 +0600

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://app.tabtracker.ai';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3NDEyNzY4MDAsCiAgImV4cCI6IDE4OTkwNDMyMDAKfQ.jExVtLyjX-PD4oem2eLllgP3Pm6KZxpQXx7c5gnl1Tc';
+const supabaseUrl = 'https://localhost:3000';
+const supabaseAnonKey = 'abcd';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
